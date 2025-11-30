ac21821ae2057c46ae54ddf72b02e4f274e47194
dDenisK <denchikkrutx2@gmail.com>
2025-11-30 13:52:10 +0200

    novobavarky

---
+++ script.js
@@ -0,0 +1,22 @@
+function resizeImageMap() {
+    const img = document.getElementById('image');
+    const map = document.getElementById('khmap');
+    const originalWidth = parseInt(img.getAttribute('data-original-width'), 10);
+    const currentWidth = img.clientWidth; 
+    const scaleFactor = currentWidth / originalWidth;
+    const areas = map.getElementsByTagName('area');
+    for (let area of areas) {
+        const originalCoordsStr = area.getAttribute('data-coords');
+        if (originalCoordsStr) {
+            const originalCoords = originalCoordsStr.split(',').map(coord => parseInt(coord.trim(), 10));
+            const newCoords = [];
+            for (let coord of originalCoords) {
+                const newCoord = Math.round(coord * scaleFactor);
+                newCoords.push(newCoord);
+            }
+            area.setAttribute('coords', newCoords.join(','));
+        }
+    }
+}
+window.addEventListener('load', resizeImageMap);
+window.addEventListener('resize', resizeImageMap);
